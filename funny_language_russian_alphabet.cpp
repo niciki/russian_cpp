d6ee39396f43da9f88f011924165f214d2860c19
niciki <71276784+niciki@users.noreply.github.com>
2021-04-29 18:33:14 +0300

    Update and rename funny_language.cpp to funny_language_russian_alphabet.cpp

---
+++ funny_language_russian_alphabet.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#define спроси std::cin 
+#define скажи std::cout
+#define оп << std::endl
+#define в >>
+#define на <<
+#define из <<
+#define _и_ &&
+#define или ||
+#define пока while(
+#define не !
+#define думай {}
+#define верни return
+#define уходи return
+#define равно ==
+#define не_равно !=
+#define если if(
+#define то )
+#define иначе else
+#define главное main()
+#define начни {
+#define закончи }
+#define четно % 2 == 0
+#define нечетно %2 != 0
+#define больше >
+#define меньше <
+#define делай )
+#define для for (
+#define накинь ++
+#define отбери --
+#define что_там_в *
+#define дальше continue
+#define забей break
+#define дай new
+#define забирай delete
+/* типы */
+#define факт bool
+#define буква char
+#define число int
+#define без_знака unsigned
+#define вещественное double
+#define строка char*
+#define фраза const char*
+#define ничего void
+#define в_факт (bool)
+#define в_число (int)
+#define в_без_знака (unsigned)
+#define в_вещественное (double)
+#define да true
+#define нет false
+
+число главное
+начни
+    число h, a, b;
+    спроси в h в a в b;
+    скажи из ((h-b)+(a-b)-1)/(a-b) оп;
+    верни 0;
+закончи
